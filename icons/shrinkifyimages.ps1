952c72be620925fc2cd0d2048dd7e3219b2e7617
irishgreencitrus <irishgreencitrus@gmail.com>
2022-06-17 14:22:14 +0100

    Added some n64 games. Redesigned Main Page

--- icons/shrinkifyimages.ps1
+++ icons/shrinkifyimages.ps1
@@ -1,4 +1,4 @@
 $files = Get-ChildItem "*.jpg"
 foreach ($f in $files) {
-    magick $f -strip -interlace Plane -quality 60% -resize "400x>" $f
+    magick $f -strip -interlace Plane -quality 65 -resize "400x>" $f
 }
