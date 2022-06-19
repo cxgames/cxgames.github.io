b55903fc3ac55c8d6bf331c3d6e0357f01c49082
irishgreencitrus <irishgreencitrus@gmail.com>
2022-06-19 18:45:06 +0100

    Added icons I forgot

--- icons/shrinkifyimages.ps1
+++ icons/shrinkifyimages.ps1
@@ -1,5 +1,12 @@
-$files = Get-ChildItem "*.jpg"
-foreach ($f in $files) {
-    magick $f -strip -interlace Plane -quality 65 -resize "400x>" $f
-    Write-Output "shrunk file: " $f
+if ($args.Count -eq 0) {
+    $files = Get-ChildItem "*.jpg"
+    foreach ($f in $files) {
+        magick $f -strip -interlace Plane -quality 65 -resize "400x>" $f
+        Write-Output "shrunk file: " $f
+    }
+} else {
+    foreach ($f in $args) {
+        magick $f -strip -interlace Plane -quality 65 -resize "400x>" $f
+        Write-Output "shrunk file: " $f
+    }
 }
